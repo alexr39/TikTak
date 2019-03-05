250e7b3b05ed680f73a4e85f8fe8f9218c602768
alex <rai>
2019-03-05 15:59:49 +0700

    game

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ def display_board(board):
     print(board[0] + '|' + board[1] + '|' + board[2])
 
 
+def marker_writer(board, index, mark):
+    board[int(index)] = mark
+    return board
+
+
 def choice_mark():
     while True:
         mark = input('Please peak a marker \'x\' or \'o\' ')
@@ -18,12 +23,48 @@ def choice_mark():
             print('You can choice only X or O')
 
 
-mark = choice_mark()
-if mark == 'x':
+def player1_turn(turns):
+    while True:
+        index = input('Player one, your turn. ')
+        if index in turns:
+            print('This cell already marked')
+        else:
+            return index
+            break
+
+def player2_turn(turns):
+    while True:
+        index = input('Player one, your turn. ')
+        if index in turns:
+            print('This cell already marked')
+        else:
+
+            return index
+            break
+
+
+board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+mark1 = choice_mark()
+if mark1 == 'x':
     mark2 = 'o'
 else:
     mark2 = 'x'
-print('Ok, let\' play')
+
+print('Ok, let\'s play. Player 1 mark is {} and player 2 - {}'.format(mark1, mark2))
+
+while True:
+    turns = []
+    index = player1_turn(turns)
+    turns.append(index)
+    board = marker_writer(board, index, mark1)
+    display_board(board)
+    index = player2_turn(turns)
+    turns.append(index)
+    board = marker_writer(board, index, mark2)
+    display_board(board)
+
+
+
 
 # def clear_board(board):
 #     for elem in board:
